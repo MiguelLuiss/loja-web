2e50133a638a54a6285317c6e7acddba4a143b1d
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-06-16 09:47:23 -0300

    arruamndo Comentarios e verificação do prouto no carrinho

--- model/carrinho.py
+++ model/carrinho.py
@@ -3,50 +3,67 @@ from data.conexao import Conexao
 
 class Carrinho:
     def adicionar_item(cod_usuario, produto):
-        conexao = Conexao.criarConexao()
-        cursor = conexao.cursor()
+            conexao = Conexao.criarConexao()
+            cursor = conexao.cursor()
 
-        sql = """
-            INSERT INTO tb_carrinho (
-                codUsuario,
-                codProduto,
-                codCategoria,
-                nome_produto,
-                descricao,
-                preco,
-                sexo,
-                url,
-                quantidade
-            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        quantidade = produto.get('quantidade', 1)  # se não tiver, usa 1 como padrão
+            sql_verifica = """
+                SELECT quantidade FROM tb_carrinho
+                WHERE codUsuario = %s AND codProduto = %s
+            """
+            cursor.execute(sql_verifica, (cod_usuario, produto['codProduto']))
+            resultado = cursor.fetchone()
 
-        cursor.execute(sql, (
-            cod_usuario,
-            produto['codProduto'],
-            produto.get('codCategoria'),
-            produto['nome_produto'],
-            produto['descricao'],
-            produto['preco'],
-            produto['sexo'],
-            produto['url'],
-            quantidade
-        ))
-        conexao.commit()
-        cursor.close()
+            if resultado:
+                nova_quantidade = resultado[0] + 1
+                sql_update = """
+                    UPDATE tb_carrinho
+                    SET quantidade = %s
+                    WHERE codUsuario = %s AND codProduto = %s
+                """
+                cursor.execute(sql_update, (nova_quantidade, cod_usuario, produto['codProduto']))
+            else:
+                sql_insert = """
+                    INSERT INTO tb_carrinho (
+                        codUsuario,
+                        codProduto,
+                        codCategoria,
+                        nome_produto,
+                        descricao,
+                        preco,
+                        sexo,
+                        url,
+                        quantidade
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """
+                cursor.execute(sql_insert, (
+                    cod_usuario,
+                    produto['codProduto'],
+                    produto.get('codCategoria'),
+                    produto['nome_produto'],
+                    produto['descricao'],
+                    produto['preco'],
+                    produto['sexo'],
+                    produto['url'],
+                    1
+                ))
+
+            conexao.commit()
+            cursor.close()
+            conexao.close()
+
 
     def mostrarCarrinho(codigo):
-        conexao = Conexao.criarConexao()
-        cursor = conexao.cursor(dictionary=True)
+            conexao = Conexao.criarConexao()
+            cursor = conexao.cursor(dictionary=True)
 
-        sql = "SELECT * FROM tb_carrinho WHERE codUsuario = %s"
-        cursor.execute(sql, (codigo,)) 
-        produto = cursor.fetchall()
+            sql = "SELECT * FROM tb_carrinho WHERE codUsuario = %s"
+            cursor.execute(sql, (codigo,)) 
+            produto = cursor.fetchall()
 
-        cursor.close()
-        conexao.close()
-        return produto
-    
+            cursor.close()
+            conexao.close()
+            return produto
+        
     def excluirItemCarrinho(codCarrinho):
         conexao = Conexao.criarConexao()
         cursor = conexao.cursor()
